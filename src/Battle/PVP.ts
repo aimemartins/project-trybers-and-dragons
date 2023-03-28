251916d0053a1728a18ae89df43619102e6d88cb
AIMÊ <aimemartins@hotmail.com>
2023-03-28 18:22:02 -0300

    comments: Iniciando classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,17 @@
+// import Character from '../Character';
+// import Battle from './Battle';
+// import Fighter from '../Fighter';
+
+// export default class PVP extends Battle {
+//   public play01: Character;
+//   public play02: Character;
+//   constructor(player01: Character, player02: Character) {
+//     super(player01);
+//     this.play01 = player01;
+//     this.play02 = player02;
+//   }
+
+//   fight(): number {
+//     return this.player.lifePoints === -1 ? -1 : 1;
+//   }
+// }
--- src/Character.ts
+++ src/Character.ts
@@ -1,75 +1,75 @@
-// import Archetype, { Mage } from './Archetypes';
-// import Energy from './Energy';
-// import Fighter from './Fighter';
-// import Race, { Elf } from './Races';
-// import getRandomInt from './utils';
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
 
-// export default class Character implements Fighter {
-//   public name: string;
-//   private _race: Race;
-//   private _archetype: Archetype;
-//   private _maxLifePoints: number;
-//   private _lifePoints: number;
-//   private _strength: number;
-//   private _defense: number;
-//   private _dexterity: number;
-//   private _energy: Energy;
+export default class Character implements Fighter {
+  public name: string;
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
 
-//   constructor(
-//     name:string,
-//   ) {
-//     this.name = name;
-//     this._dexterity = getRandomInt(1, 10);
-//     this._race = new Elf(name, this.dexterity);
-//     this._archetype = new Mage(name);
-//     this._maxLifePoints = this._race.maxLifePoints / 2;
-//     this._lifePoints = this._maxLifePoints;
-//     this._strength = getRandomInt(1, 10);
-//     this._defense = getRandomInt(1, 10);
-//     this._energy = {
-//       type_: this._archetype.energyType,
-//       amount: getRandomInt(1, 10),
-//     };
-//   }
+  constructor(
+    name:string,
+  ) {
+    this.name = name;
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this.dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
 
-//   get race():Race {
-//     return this._race;
-//   }
+  get race():Race {
+    return this._race;
+  }
 
-//   get archetype():Archetype {
-//     return this._archetype;
-//   }
+  get archetype():Archetype {
+    return this._archetype;
+  }
 
-//   get lifePoints():number {
-//     return this._lifePoints;
-//   }
+  get lifePoints():number {
+    return this._lifePoints;
+  }
 
-//   get strength():number {
-//     return this._strength;
-//   }
+  get strength():number {
+    return this._strength;
+  }
 
-//   get defense():number {
-//     return this._defense;
-//   }
+  get defense():number {
+    return this._defense;
+  }
 
-//   get dexterity():number {
-//     return this._dexterity;
-//   }
+  get dexterity():number {
+    return this._dexterity;
+  }
 
-//   get energy():Energy {
-//     return this._energy;
-//   }
+  get energy():Energy {
+    return this._energy;
+  }
 
-//   receiveDamage(attackPoints:number):number {
-//     const damage = attackPoints - this._defense;
-//     return damage;
-//   }
+  receiveDamage(attackPoints:number):number {
+    const damage = attackPoints - this._defense;
+    return damage;
+  }
 
-//   attack(enemy: Fighter):void {
-//     enemy.receiveDamage(this._strength);
-//   }
+  attack(enemy: Fighter):void {
+    enemy.receiveDamage(this._strength);
+  }
 
-//   levelUp():void;
+  levelUp():void;
 
-//   special?(enemy: Fighter):void;
-// }
+  special?(enemy: Fighter):void;
+}
